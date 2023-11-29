fc824f39905aaf2238987346ecfb575101d5d4f5
bhard <sendtoashish123@gmail.com>
2023-11-29 11:46:30 +0530

    cpmmiting changes

---
+++ placement-engine-authentication/src/main/java/placementengineauthenticationservice/PlacementEngineAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package placementengineauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementEngineAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementEngineAuthenticationApplication.class, args);
+	}
+
+}
---
+++ placement-engine-authentication/src/test/java/placementengineauthenticationservice/PlacementEngineAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package placementengineauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementEngineAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
